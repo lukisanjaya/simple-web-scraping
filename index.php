971809baafaca4e714a43e7fe340bb5d43398611
lukisanjaya <ukykazuki@gmail.com>
2017-10-06 17:24:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,6 @@ $html = $url->outerHtml;
 $html = HtmlDomParser::str_get_html($html);
 
 $content = $html->find('div.single-blog-content', 0)->innertext;
-$content = str_replace('<h3>Related posts:</h3>', '', $content);
-$content = str_replace($html->find('div[style=border: 0pt none ; margin: 0pt; padding: 0pt;]', 0)->outertext, '', $content);
 $publishDate = $html->find('meta[property=article:published_time]', 0)->content;
 
 foreach ($html->find('a[rel=category tag]') as $val) :
@@ -21,7 +19,6 @@ foreach ($html->find('a[rel=category tag]') as $val) :
 endforeach;
 
 $data = [
-	'link'       => $html->find('link[rel=canonical]', 0)->href,
 	'publish_at' => date("Y-m-d", strtotime($publishDate)),
 	'title'      => $html->find('h1', 0)->plaintext,
 	'img'        => $html->find('img.size-full', 0)->src,
@@ -30,8 +27,5 @@ $data = [
 	'category'	 => $category,
 
 ];
-// innertext
 
-// $content = str_replace(search, replace, subject)
 var_dump($data);
-// var_dump($html->find('div[style=border: 0pt none ; margin: 0pt; padding: 0pt;]', 0)->outertext);
